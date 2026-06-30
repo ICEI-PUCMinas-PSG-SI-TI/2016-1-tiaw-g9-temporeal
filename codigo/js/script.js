ddfed051f54b3f85947b84f09bce04fb7cd4284f
Luiz Henrique <1658728@sga.pucminas.br>
2026-06-29 21:47:12 -0300

    Atualizações no login e cadastro, home do aluno e página do professor

--- codigo/js/script.js
+++ codigo/js/script.js
@@ -1,57 +1,305 @@
-// script.js - Luiz (Login, Cadastro e logout)
+function cadastrar(){
 
-function cadastrar() {
-    const senha = document.getElementById("senha").value;
-    const confirmarSenha = document.getElementById("confirmarSenha").value;
 
-    if (senha !== confirmarSenha) {
-        //alert("As senhas são diferentes!");
-        return;
-    }
+const senha =
+document.getElementById("senha").value;
 
-    const usuario = {
-        nome: document.getElementById("nome").value,
-        telefone: document.getElementById("telefone").value,
-        cidade: document.getElementById("cidade").value,
-        categoria: document.getElementById("categoria").value,
-        escola: document.getElementById("escola").value,
-        email: document.getElementById("email").value,
-        senha: senha
-    };
 
-    localStorage.setItem("usuario", JSON.stringify(usuario));
-    //alert("Cadastro realizado com sucesso!");
-    window.location.href = "index.html";
+
+const confirmarSenha =
+document.getElementById("confirmarSenha").value;
+
+
+
+
+if(senha !== confirmarSenha){
+
+
+alert("As senhas são diferentes!");
+
+return;
+
+
 }
 
-function login() {
-    const email = document.getElementById("emailLogin").value;
-    const senha = document.getElementById("senhaLogin").value;
-    const usuario = JSON.parse(localStorage.getItem("usuario"));
 
-    if (usuario && usuario.email === email && usuario.senha === senha) {
-        
-        localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
-        window.location.href = "home.html";
-    } else {
-        //alert("Email ou senha incorretos!");
-    }
+
+
+const usuario = {
+
+
+id: Date.now(),
+
+
+
+nome:
+document.getElementById("nome").value,
+
+
+
+telefone:
+document.getElementById("telefone").value,
+
+
+
+cidade:
+document.getElementById("cidade").value,
+
+
+
+tipo:
+document.getElementById("categoria").value.toLowerCase(),
+
+
+
+escola:
+document.getElementById("escola").value,
+
+
+
+email:
+document.getElementById("email").value,
+
+
+
+senha
+
+};
+
+
+
+
+
+let usuarios =
+
+JSON.parse(localStorage.getItem("usuarios")) || [];
+
+
+
+
+
+const existe =
+
+usuarios.find(usuarioExistente =>
+
+usuarioExistente.email === usuario.email
+
+);
+
+
+
+
+if(existe){
+
+
+alert("Este email já está cadastrado!");
+
+
+return;
+
+
 }
 
-function logout() {
-    localStorage.removeItem("usuarioLogado");
-    window.location.href = "index.html";
+
+
+
+usuarios.push(usuario);
+
+
+
+
+
+
+localStorage.setItem(
+
+"usuarios",
+
+JSON.stringify(usuarios)
+
+);
+
+
+
+window.location.href="index.html";
+
+
+
 }
 
-// coloca nome do usuário na home
-const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
-if (usuarioLogado) {
-    const mensagem = document.getElementById("mensagem");
-    if (mensagem) mensagem.innerHTML = "Bem-vindo, " + usuarioLogado.nome + "!";
 
-    const nomeUsuario = document.getElementById("nomeUsuario");
-    if (nomeUsuario) nomeUsuario.textContent = usuarioLogado.nome;
 
-    const tipoUsuario = document.getElementById("tipoUsuario");
-    if (tipoUsuario) tipoUsuario.textContent = usuarioLogado.categoria || "";
+function login(){
+
+
+
+const email =
+
+document.getElementById("emailLogin").value;
+
+
+
+
+
+const senha =
+
+document.getElementById("senhaLogin").value;
+
+
+
+
+
+
+const usuarios =
+
+JSON.parse(localStorage.getItem("usuarios")) || [];
+
+
+
+
+
+
+
+const usuario =
+
+usuarios.find(usuario =>
+
+usuario.email === email &&
+
+usuario.senha === senha
+
+);
+
+
+
+
+if(usuario){
+
+
+
+
+
+
+localStorage.setItem(
+
+"usuarioLogado",
+
+JSON.stringify(usuario)
+
+);
+
+
+
+
+if(usuario.tipo === "professor"){
+
+
+
+window.location.href="professor.html";
+
+
+
+}
+
+else if(usuario.tipo === "responsavel"){
+
+
+
+window.location.href="responsavel.html";
+
+
+
+}
+
+else{
+
+
+
+window.location.href="home.html";
+
+
+}
+
+
+
+
+
+}
+
+else{
+
+
+
+alert("Email ou senha incorretos!");
+
+
+
+}
+
+
+
+}
+
+
+
+const usuarioLogado =
+
+JSON.parse(localStorage.getItem("usuarioLogado"));
+
+
+
+
+if(usuarioLogado){
+
+
+
+const nomeUsuario =
+
+document.getElementById("nomeUsuario");
+
+
+
+if(nomeUsuario){
+
+
+nomeUsuario.innerHTML =
+usuarioLogado.nome;
+
+
+}
+
+
+
+
+const tipoUsuario =
+
+document.getElementById("tipoUsuario");
+
+
+
+if(tipoUsuario){
+
+
+tipoUsuario.innerHTML =
+usuarioLogado.tipo;
+
+
+}
+
+
+
+}
+
+
+
+
+
+function logout(){
+
+
+localStorage.removeItem("usuarioLogado");
+
+
+window.location.href="index.html";
+
+
 }
